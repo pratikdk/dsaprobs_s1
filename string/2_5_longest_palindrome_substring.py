4392538c1fcb6dd7d4dbc2674f1f225bd4d3e41f
pratikdk <pratik.deoolwadikar@gmail.com>
2020-11-21 22:06:59 +0530

    Day two

---
+++ string/2_5_longest_palindrome_substring.py
@@ -0,0 +1,25 @@
+def longest_palindrome(s):
+    start = 0
+    end = 0
+    for i in range(len(s)):
+        # Capture odd palindrome at i
+        len1 = expand_around_center(s, i, i)
+        # Capture even palindrome at i
+        len2 = expand_around_center(s, i, i+1)
+        # Consider palindrom with maximum len
+        length = max(len1, len2)
+        # Update start and end indices to max palindrome
+        if (length > end-start): # if new max len
+            start = i - (length-1)//2
+            end = i + length//2
+    return s[start:(end+1)]
+
+def expand_around_center(s, left, right):
+    L = left
+    R = right
+    while (L >= 0) and (R < len(s)) and (s[L] == s[R]):
+        L -= 1
+        R += 1
+    return R-L-1
+
+print(longest_palindrome("fabbac"))

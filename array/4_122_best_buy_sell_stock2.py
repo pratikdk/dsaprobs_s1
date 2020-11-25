d3955206296aa957ee724cdf19385b3c4f2a4eb5
pratikdk <pratik.deoolwadikar@gmail.com>
2020-11-25 16:05:07 +0530

    Day five

---
+++ array/4_122_best_buy_sell_stock2.py
@@ -0,0 +1,40 @@
+# def maxProfit(prices):
+#     min_price = float('inf')
+#     max_profit = 0
+#     n = len(prices)
+#     for i, price in enumerate(prices):
+#         if price < prices[i-1]: # Breakaway from loss
+#             if min_price < prices[i-1]: # but firt Capture prof
+#                 max_profit += prices[i-1]-min_price
+#             min_price = price
+#         if (i==n-1) and (price>min_price):
+#             max_profit += price-min_price
+#     return max_profit
+
+def maxProfit(prices):
+    if len(prices) < 1: return 0
+    min_price = prices[0]
+    max_profit = 0
+    n = len(prices)
+    for i, price in enumerate(prices):
+        if i == 0: continue
+        if price < prices[i-1]: # Breakaway from loss
+            if min_price < prices[i-1]: # but firt Capture prof
+                max_profit += prices[i-1]-min_price
+            min_price = price
+        if (i==n-1) and (price>min_price):
+            max_profit += price-min_price
+    return max_profit
+
+data = [
+    [7,1,5,3,6,4],
+    [1,2,3,4,5],
+    [7,6,4,3,1],
+    [7,1,5,0,3,6,2,8],
+    [2, 4, 1],
+    [5],
+    []
+]
+
+for seq in data:
+    print(maxProfit(seq))

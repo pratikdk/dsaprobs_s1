e95230889080f0e4901fb054d8c4eae82f7ed490
pratikdk <pratik.deoolwadikar@gmail.com>
2020-12-07 04:11:25 +0530

    Day 11

---
+++ linked_list/10_237_delete_node.py
@@ -0,0 +1,28 @@
+from linked_list import ListNode, nodelist_builder, printNodes
+
+
+def delete(node):
+    print("Node here is:", node.val)
+    if node != None and node.next != None:
+        node.val = node.next.val
+        node.next = node.next.next
+
+
+if __name__ == "__main__":
+    data = [
+        ([4, 5, 1, 9], 5),
+        ([4, 5, 1, 9], 1),
+        ([1, 2, 3, 4], 3),
+        ([0, 1], 0),
+        ([-3, 5, -99], -3)
+    ]
+    for test_list in data:
+        head = nodelist_builder(test_list[0])
+        dnode = head
+        while dnode != None:
+            if dnode.val == test_list[1]:
+                break
+            dnode = dnode.next
+        delete(dnode)
+        printNodes(head)
+        print("---x---")

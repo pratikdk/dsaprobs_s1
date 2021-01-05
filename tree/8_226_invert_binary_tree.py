1462455814ef4198315b3750457ed2d6c47a6124
pratikdk <pratik.deoolwadikar@gmail.com>
2021-01-05 16:41:26 +0530

    Day 16

---
+++ tree/8_226_invert_binary_tree.py
@@ -0,0 +1,48 @@
+from tree_node import TreeNode
+
+# def invertTree(root):
+#     queue = []
+#     queue.append(root)
+#     while queue:
+#         buffer = []
+#         for node in queue:
+#             if node.left:
+#                 buffer.append(node.left)
+#             if node.right:
+#                 buffer.append(node.right)
+#         buffer = reversed(buffer)
+#         for node in queue:
+def invertTree(root):
+    queue = []
+    queue.append(root)
+    while queue and root:
+        node = queue.pop(0)
+        node.left, node.right = node.right, node.left
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    return root
+
+
+def preorder(root):
+    return [root.value] + preorder(root.left) + preorder(root.right) if root else []
+
+
+if __name__ == "__main__":
+    n1 = TreeNode(4)
+    n2 = TreeNode(2)
+    n3 = TreeNode(7)
+    n4 = TreeNode(1)
+    n5 = TreeNode(3)
+    n6 = TreeNode(6)
+    n7 = TreeNode(9)
+    n1.left = n2
+    n1.right = n3
+    n2.left = n4
+    n2.right = n5
+    n3.left = n6
+    n3.right = n7
+    print(preorder(n1))
+    print(preorder(invertTree(n1)))
+    print(preorder(invertTree(None)))
